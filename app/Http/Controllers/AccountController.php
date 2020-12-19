8475f0097df5c7295d9e983086c837a981236579
Simon Duun <simon@scratcher.io>
2020-12-19 20:14:14 +0100

    Add account and transaction controller logic

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Account;
+//use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class AccountController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return string
+     */
+    public function index()
+    {
+        return Account::all()->toJson();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validation failed',
+                'validation' => $validator->getMessageBag()->toJson(),
+            ], 422);
+        }
+
+        $account = Account::create($request->all());
+
+        if ($account) {
+            return response()->json([
+                'message' => 'Account created successfully',
+                'account_name' => $request->name,
+                'account_id' => $account->id
+            ], 201);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show(Request $request)
+    {
+        if (!Account::where(['id' => request()->accountId])->exists()) {
+            return response()->json([
+                'message' => 'No account found',
+                'validation' => 'The entered account id does not match any active accounts',
+            ], 404);
+        }
+        $account = Account::find(request()->accountId);
+        return $account->transactions()->get()->toJson();
+    }
+}
---
+++ app/Http/Controllers/TransactionController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Account;
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class TransactionController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'description' => 'required',
+            'amount' => 'required|numeric'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validation failed',
+                'validation' => $validator->getMessageBag()->toJson(),
+            ], 422);
+        }
+
+        if(!Account::where(['id' => request()->accountId])->exists()) {
+            return response()->json([
+                'message' => 'No account found',
+                'validation' => 'The entered account id does not match any active accounts',
+            ], 404);
+        }
+
+        if (Transaction::create([
+            'description' => $request->description,
+            'amount' => $request->amount,
+            'account_id' => request()->accountId,
+        ])) {
+            return response()->json([
+                'message' => 'Transaction created successfully',
+                'transaction' => [
+                    'description' => $request->description,
+                    'amount' => $request->amount,
+                ],
+            ], 201);
+        }
+    }
+}
