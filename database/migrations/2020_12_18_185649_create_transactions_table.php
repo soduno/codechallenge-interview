d54a0c1ac688a37082dc31bce959d7d6f758d9e4
Simon Duun <simon@scratcher.io>
2020-12-19 20:14:45 +0100

    add migrations

---
+++ database/migrations/2020_12_18_185649_create_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('account_id');
+            $table->text('description');
+            $table->decimal('amount');
+            $table->timestamps();
+
+            $table->index('account_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
